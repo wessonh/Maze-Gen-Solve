15a838839fa20ec9020d1fc0a014862a05a20e23
wessonh <80785364+wessonh@users.noreply.github.com>
2023-05-20 13:25:02 -0700

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,59 +1,57 @@
-
 package application;
-import java.util.Collections;
-import java.util.Stack;
-
-import javafx.application.Application; 
-import javafx.stage.Stage;
+import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.shape.Line;
-
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Main extends Application {
-	
-	private static Group group = new Group();
-	private static Group group2 = new Group();
-	private Stage stage = new Stage();
-	private Stage stage2 = new Stage();
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			stage.setX(300);
-			stage.setY(300);
-			stage2.setX(700);
-			stage2.setY(300);
-			Scene scene = new Scene(group,400,400);
-			Scene scene2 = new Scene(group2,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			stage.setScene(scene);
-			stage2.setScene(scene2);
-			stage2.show();
-			stage.show();
-			
-		} 
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		
-		Node [][] maze = new Node [10][10];
-		for(int i = 0; i < 10; i++) {
-			
-			for(int k = 0; k < 10; k++) {
-				
-				maze[i][k] = new Node(i, k);
 
-			}
-		}
-		Gen generate = new Gen();
-		generate.create(group,maze,10,10);
-		Solve solve = new Solve();
-		solve.create(group2,maze,10,10);
-		launch(args);
-	}
-	
-	
+    static final Group group = new Group(); // group to store nodes for scene display
+    static Node[][] maze;
+    AtomicBoolean solverDone = new AtomicBoolean(false); // boolean values that can...
+    AtomicBoolean mazeGenerated = new AtomicBoolean(false); // ...update automatically...
+
+    @Override // entry point for javaFX                   // ...good for using them for multiple things
+    public void start(Stage primaryStage) {
+
+        try {
+            Label instructions = Menu.instructions();
+            group.getChildren().add(instructions); // Add the instructions to the group
+
+            Scene scene = new Scene(group, 425, 425); // creates a new scene with group as the root, this is where you adjust window size
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("application.css")).toExternalForm()); // adds the CSS style sheet
+            primaryStage.setScene(scene); // sets primary stage to our scene
+            primaryStage.show(); // displays the window
+
+            scene.setOnKeyPressed(event -> {
+                if (event.getCode() == KeyCode.Q) {
+                    System.exit(0);
+                } else if (!mazeGenerated.get()) {  // If the maze is not generated
+                    instructions.setVisible(false);
+                    maze = Gen.animateGen(group);
+                    mazeGenerated.set(true);
+                } else if (mazeGenerated.get() && !solverDone.get()) {  // If the maze is generated and not solved
+                    instructions.setVisible(false);
+                    Solve solver = new Solve();
+                    solver.animateSolve(maze, group, solverDone);
+                } else if (mazeGenerated.get() && solverDone.get()) {  // If the maze is generated and solved
+                    group.getChildren().clear(); // clear all nodes from group
+                    solverDone.set(false); // reset the solver finished flag
+                    mazeGenerated.set(false); // set maze generated to false, but don't generate a new maze yet
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        maze = new Node[0][0]; // might change this, or try to remove it 
+        launch(args);
+    }
 }
